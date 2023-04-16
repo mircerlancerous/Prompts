bfc875da1f5919c36bf597598ba15f62b8efbb2c
Dave <mircerlancerous@users.noreply.github.com>
2023-04-16 16:33:57 -0700

    Create Prompts.js
    Initial commit - library in functional state

---
+++ Prompts.js
@@ -0,0 +1,285 @@
+/*
+Copyright 2023 OffTheBricks - https://github.com/mircerlancerous/Prompts
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+ http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+var Prompts = new function(){
+	this.alert = function(msg){
+		return new Promise(function(resolve,reject){
+			let newDiv = createBox(msg);
+			let newBtn = document.createElement("div");
+			newBtn.className = "promptButton";
+			newBtn.innerText = "OK";
+			newBtn.addEventListener("click", function(){closePrompt();resolve();}, false);
+			newDiv.appendChild(newBtn);
+			deployPrompt(newDiv);
+			//needs to come after prompt is deployed
+			_currentPromiseReject = reject;
+		});
+	};
+	
+	this.confirm = function(msg){
+		return new Promise(function(resolve,reject){
+			let newDiv = createBox(msg);
+			let newBtn = document.createElement("div");
+			newBtn.className = "promptButton half";
+			newBtn.innerText = "OK";
+			newBtn.addEventListener("click", function(){closePrompt();resolve(true);}, false);
+			newDiv.appendChild(newBtn);
+			newBtn = document.createElement("div");
+			newBtn.className = "promptButton half";
+			newBtn.innerText = "CANCEL";
+			newBtn.addEventListener("click", function(){closePrompt();resolve(false);}, false);
+			newDiv.appendChild(newBtn);
+			deployPrompt(newDiv);
+			//needs to come after prompt is deployed
+			_currentPromiseReject = reject;
+		});
+	};
+	
+	this.prompt = function(msg, defaultvalue, placeholder){
+		return new Promise(function(resolve,reject){
+			let newDiv = createBox(msg);
+			let newInput = document.createElement("input");
+			newInput.type = "text";
+			newInput.className = "promptInput";
+			if(defaultvalue){
+				newInput.value = defaultvalue;
+			}
+			if(placeholder){
+				newInput.placeholder = placeholder;
+			}
+			newDiv.appendChild(newInput);
+			let newBtn = document.createElement("div");
+			newBtn.className = "promptButton half";
+			newBtn.innerText = "OK";
+			newBtn.addEventListener(
+				"click",
+				function(){
+					closePrompt();
+					let valueElm = newDiv.getElementsByClassName("promptInput")[0];
+					resolve(valueElm.value);
+				},
+				false
+			);
+			newDiv.appendChild(newBtn);
+			newBtn = document.createElement("div");
+			newBtn.className = "promptButton half";
+			newBtn.innerText = "CANCEL";
+			newBtn.addEventListener("click", function(){closePrompt();resolve(false);}, false);
+			newDiv.appendChild(newBtn);
+			deployPrompt(newDiv);
+			newInput.focus();
+			//needs to come after prompt is deployed
+			_currentPromiseReject = reject;
+		});
+	};
+	
+	this.waiting = function(msg){
+		//if no message, then close prompt
+		if(!msg){
+			closePrompt();
+			return;
+		}
+		//have message, so show prompt with no buttons
+		let newDiv = createBox(msg);
+		let newWaiting = document.createElement("div");
+		newWaiting.className = "promptWaiting";
+		newWaiting.innerText = ".";
+		newDiv.appendChild(newWaiting);
+		deployPrompt(newDiv);
+		//start timer for waiting animation
+		promptTimer = setInterval(onPromptWaiting, 500);
+	};
+	
+	this.timed = function(msg, ms){
+		if(!ms){
+			//default to 2.5 seconds if a value is not provided
+			ms = 2500;
+		}
+		return new Promise((resolve, reject)=>{
+			let newDiv = createBox(msg);
+			deployPrompt(newDiv);
+			promptTimeout = setTimeout(
+				function(){
+					closePrompt();
+					resolve();
+				},
+				ms
+			);
+			//needs to come after prompt is deployed
+			_currentPromiseReject = reject;
+		});
+	};
+
+	this.isOpen = function(){
+		if(promptElms.length > 0){
+			return true;
+		}
+		return false;
+	};
+	
+	//closes any open prompt prematurely, rejecting its promise
+	this.close = function(){
+		forceClose();
+	};
+	
+	//if enabled, prompts are automatically closed when a new prompt is requested
+	this.setAutoClose = function(enable){
+		_autoClose = enable;
+	};
+	
+	/*******************************/
+	
+	var _autoClose = false;
+	var _currentPromiseReject = null;
+	
+	var _style = {
+		promptShade: {
+			"z-index": 1000,
+			height: "100%",
+			width: "100%",
+			position: "fixed",
+			top: "0px",
+			left: "0px",
+			opacity: "50%",
+			"background-color": "black",
+			"pointer-events": "none"
+		},
+		promptHolder: {
+			"z-index": 1001,
+			width: "100%",
+			position: "fixed",
+			top: "100px",
+			left: "0px",
+			"text-align": "center",
+			promptBox: {
+				display: "inline-block",
+				width: "90%",
+				"max-width": "350px",
+				"background-color": "white",
+				padding: "15px",
+				"font-size": "14px",
+				border: "3px solid blue",
+				"box-shadow": "0px 0px 25px",
+				promptButton: {
+					margin: "10px",
+					padding: "5px",
+					border: "1px solid blue",
+					"font-size": "18px",
+					"box-shadow": "0px 0px 10px black",
+					"border-radius": "4px"
+				},
+				promptInput: {
+					border: "1px solid #aaa",
+					padding: "3px;",
+					margin: "10px;",
+					display: "inline-block",
+					width: "80%",
+					height: "25px",
+					"margin-bottom": "15px"
+				},
+				promptWaiting: {
+					"font-size": "20px"
+				}
+			}
+		},
+		half: {
+			display: "inline-block",
+			width: "100px"
+		}
+	};
+	
+	function objToStyle(obj, prefix){
+		let i, style = "", substyle = "", list = Object.getOwnPropertyNames(obj);
+		if(!prefix){
+			prefix = "";
+		}
+		for(i=0; i<list.length; i++){
+			if(typeof(obj[list[i]]) === 'object'){
+				substyle += objToStyle(obj[list[i]], prefix + " ." + list[i]);
+			}
+			else{
+				style += list[i] + ": " + obj[list[i]] + ";";
+			}
+		}
+		if(style.length){
+			style = prefix + "{" + style + "}";
+		}
+		return style + substyle;
+	}
+	
+	function createBox(msg){
+		let newDiv = document.createElement("div");
+		newDiv.className = "promptBox";
+		newDiv.innerText = msg + "\n\n";
+		return newDiv;
+	}
+	
+	function deployPrompt(newDiv){
+		if(promptElms.length > 0){
+			if(!_autoClose){
+				throw new Error("Prompt already open");
+			}
+			forceClose();
+		}
+		let newShade = document.createElement("div");
+		newShade.className = "promptShade";
+		promptElms.push(newShade);
+		document.body.appendChild(newShade);
+		let newHolder = document.createElement("div");
+		newHolder.className = "promptHolder";
+		newHolder.appendChild(newDiv);
+		promptElms.push(newHolder);
+		document.body.appendChild(newHolder);
+		let newStyle = document.createElement("style");
+		newStyle.innerHTML = objToStyle(_style);
+		promptElms.push(newStyle);
+		document.body.appendChild(newStyle);
+	}
+	
+	var promptElms = [], promptTimer = null, promptTimeout = null;
+	function closePrompt(){
+		for(let i=0; i<promptElms.length; i++){
+			document.body.removeChild(promptElms[i]);
+		}
+		promptElms = [];
+		_currentPromiseReject = null;
+		if(promptTimer){
+			clearInterval(promptTimer);
+			promptTimer = null;
+		}
+		if(promptTimeout){
+			clearTimeout(promptTimeout);
+			promptTimeout = null;
+		}
+	}
+	
+	function forceClose(){
+		if(_currentPromiseReject){
+			_currentPromiseReject(new Error("Close prompt called"));
+		}
+		closePrompt();
+	}
+	
+	function onPromptWaiting(){
+		let elm = document.getElementsByClassName("promptHolder")[0];
+		elm = elm.getElementsByClassName("promptWaiting")[0];
+		if(elm.innerText.length > 20){
+			elm.innerText = ".";
+		}
+		else{
+			elm.innerText = elm.innerText + " . .";
+		}
+	}
+};
